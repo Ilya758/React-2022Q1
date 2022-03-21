4369f04d73f04c932fbc24849bc2bc61f14a9492
Ilya758 <subscriber6436@gmail.com>
2022-03-21 19:48:56 +0300

    feat: added sass into dependencies, implemented component Header

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Header } from './components/Header';
+import './App.scss';
 
 export class App extends React.Component {
   render() {
-    return <h1>Test</h1>;
+    return <Header />;
   }
 }

451722eb798e9e0c3980d14fb545468a6be015b0
Ilya758 <subscriber6436@gmail.com>
2022-03-21 21:15:39 +0300

    feat: implemented basic router structure

--- react-components/src/App.tsx
+++ react-components/src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import { Header } from './components/Header';
 import './App.scss';
+import { Route, Routes } from 'react-router';
+import { HomePage } from './components/HomePage/HomePage';
+import { AboutPage } from './components/AboutPage/AboutPage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 
 export class App extends React.Component {
   render() {
-    return <Header />;
+    return (
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    );
   }
 }

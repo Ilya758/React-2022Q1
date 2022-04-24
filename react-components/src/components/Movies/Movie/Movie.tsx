df1cbb59fcfc2ce56541db3193a165b12a57fd55
Ilya758 <subscriber6436@gmail.com>
2022-04-24 17:57:51 +0300

    fix: fixed component Movie with AppContext, fixed styles

--- react-components/src/components/Movies/Movie/Movie.tsx
+++ react-components/src/components/Movies/Movie/Movie.tsx
@@ -1,16 +1,31 @@
 import { TMovie } from './Movie.types';
 import './Movie.scss';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../../../global/context/appContext';
+import { TAppProps } from '../../../App.types';
+import { setDetailedPageMovie } from '../../../store/reducers/actionCreators';
 
-const Movie = ({ nameOriginal, posterUrl, type, year }: TMovie) => {
+const Movie = ({ nameOriginal, posterUrl, type, year, kinopoiskId, ratingImdb }: TMovie) => {
+  const { dispatch } = useContext(AppContext) as TAppProps;
+
+  const setMovieToDetailedPage = () => {
+    dispatch(
+      setDetailedPageMovie({ nameOriginal, posterUrl, type, year, kinopoiskId, ratingImdb })
+    );
+  };
+
   return (
     <div className="card">
-      <img className="card__img" src={posterUrl} alt={nameOriginal} />
+      <Link onClick={setMovieToDetailedPage} className="card__link" to={`movies/${kinopoiskId}`}>
+        <img className="card__img" src={posterUrl} alt={nameOriginal} />
 
-      <div className="card__content">
-        <h1>{nameOriginal}</h1>
-        <h3>Year: {year}</h3>
-        <h3>Type: {type}</h3>
-      </div>
+        <div className="card__content">
+          <h1>{nameOriginal}</h1>
+          <h3>Year: {year}</h3>
+          <h3>Type: {type}</h3>
+        </div>
+      </Link>
     </div>
   );
 };
